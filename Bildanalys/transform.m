0b61581a8f73d09ba7249d4338c06735810c83c4
Joel Sjobom <glajsnake@hotmail.com>
2013-05-09 15:52:05 +0200

    fixed transformation for new camera position

--- Bildanalys/transform.m
+++ Bildanalys/transform.m
@@ -1,12 +1,10 @@
-function transfim = transform(im);
+function transfim = transform(im)
+
 
 [width height] = size(im);
 
-A = [1 -15/height 0.14/height
-     610/width 1 0.8/width
+A = [1 -15/height -0.09/height
+     -150/width 1 -0.22/width
      0 0 1];
 T = maketform('projective', A);
 transfim = imtransform(im, T);
-
-transfim = flipdim(transfim,1);
-transfim = flipdim(transfim,2);
