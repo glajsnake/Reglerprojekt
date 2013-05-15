adb06b7e6ccd80b50fed22425adaf9728b3ced74
Joel Sjobom <glajsnake@hotmail.com>
2013-05-15 17:34:34 +0200

    removed manual exposura and brightness

--- Bildanalys/prepare_camera.m
+++ Bildanalys/prepare_camera.m
@@ -15,12 +15,12 @@ end
 
 vid = videoinput('winvideo',camnum,video.SupportedFormats{1})
 source = getselectedsource(vid);
-set(source,'ExposureMode','manual')
+%set(source,'ExposureMode','manual')
 set(source,'FocusMode','manual')
-set(source,'WhiteBalanceMode','manual')
+%set(source,'WhiteBalanceMode','manual')
 source.focus = 143;
-source.exposure = -3;
-source.WhiteBalance = 6000;
+%source.exposure = -3;
+%source.WhiteBalance = 6000;
 source.brightness = 170;
 source.horizontalflip = 'on';
 source.verticalflip = 'on';
